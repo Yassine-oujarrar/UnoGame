2a36a58264122572f6466fe256896567e177c1ae
Saurelle Ngueuchue Nankam <ngueuchu@uqtr.ca>
2023-10-17 03:11:13 +0000

    Upload New File

---
+++ PileDeDepot.cs
@@ -0,0 +1,25 @@
+﻿public class PileDeDepot : PileDeCartes
+{
+    public PileDeDepot(List<Carte> cartes) : base(cartes) { }
+    public void AjouterUneCarte(Carte carte)
+    {
+        Cartes.Add(carte);
+    }
+
+    public Carte DerniereCarte()
+    {
+        if (Cartes.Count > 0)
+        {
+            return Cartes[Cartes.Count - 1];
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    internal void Add(Carte carte)
+    {
+        throw new NotImplementedException();
+    }
+}
